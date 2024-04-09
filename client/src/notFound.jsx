f04f039b0fb7723bdf9250c27a751553e97157bd
SrizaBiswas <biswassriza76@gmail.com>
2024-04-09 15:38:26 +0530

    first commit

---
+++ client/src/notFound.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const notFound = () => {
+  return (
+    <div className="w-full h-screen bg-white dark:bg-black flex items-center justify-center">
+      Your search did not match any Media
+    </div>
+  );
+};
+
+export default notFound;
